7f451d5ea5e65a746822e5f79e08ce78efb6839c
Taiane Serrão <contato.taianeserrao@gmail.com>
2026-05-14 14:54:09 -0400

    Initial commit - Banner criation

---
+++ apache-log-analysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+banner()
+{
+        echo ".................................."
+        echo "         Análise de Logs          "
+        echo ".................................."
+}
+
+banner2()
+{
+        echo "                            By Tai"
+}
+
+
+banner
+banner2
